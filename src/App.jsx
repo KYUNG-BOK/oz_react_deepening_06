c66240ca9cee987313c7acfbc493b8e6c08d1014
kyeongbok <kyeongbok_0627@kakao.com>
2025-07-03 12:52:10 +0900

    리액트6일차 도전과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css';
+  //1. React Router의 `BrowserRouter`를 설정해야 합니다.
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/admin/Home';
 import Layout from './pages/Layout';
 import Dashboard from './pages/admin__dashboard/Dashboard';
@@ -8,15 +10,32 @@ import Settings from './pages/admin__settings/Settings';
 import NotFound from './pages/admin__notfound/NotFound';
 
 function App() {
-  //1. React Router의 `BrowserRouter`를 설정해야 합니다.
-  //2. 메인 페이지에 Home 컴포넌트를 렌더링 해야 합니다.
-  //3. /admin에서 Dashboard를 렌더링 해야 합니다. 이는 모든 /admin 경로에서 보입니다.
-  //4. /admin/users에서 Users를 렌더링 해야 합니다.
-  //5. /admin/users/:id에서 UsersDetail를 렌더링 해야 합니다.
-  //6. /admin/settings에서 Settings를 렌더링 해야 합니다.
-  //7. /admin/<잘못된 페이지명>에서 NotFound를 렌더링 해야 합니다.
+  return(
+    <BrowserRouter>
+      <Routes>
+        {/*2. 메인 페이지에 Home 컴포넌트를 렌더링 해야 합니다.*/}
+        <Route path="/" element={<Home />} />
 
-  return;
+        {/* /admin` 경로에 Layout 레이아웃을 적용합니다. */}
+        
+        <Route path="/admin" element={<Layout />}>
+          {/* 3. /admin에서 Dashboard를 렌더링 해야 합니다. 이는 모든 /admin 경로에서 보입니다. */}
+          <Route index element={<Dashboard />} />
+
+          {/* /admin/users에서 Users를 렌더링 해야 합니다. */}
+          <Route path="users" element={<Users />} />
+          {/* /admin/users/:id에서 UsersDetail를 렌더링 해야 합니다. */}
+          <Route path="users/:id" element={<UserDetail />} />
+          {/* /admin/settings에서 Settings를 렌더링 해야 합니다. */}
+          <Route path="settings" element={<Settings />} />
+
+          {/* /admin 이하 잘못된 주소 */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+
 }
 
 export default App;
